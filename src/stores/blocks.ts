2b71c6eff119484b256679426e7fe937c394ed28
santiago <ry.yrupailla@gmail.com>
2025-02-07 00:42:25 -0500

    Something

--- src/stores/blocks.ts
+++ src/stores/blocks.ts
@@ -22,6 +22,12 @@ export const useBlocksStore = defineStore("blocks", () => {
         {
           text: "(<c d4>2 c'x20 c'')",
         },
+        {
+          text: "(c1x3)",
+        },
+        {
+          text: "(c1x3)",
+        },
       ],
     },
   ]);
@@ -30,3 +36,5 @@ export const useBlocksStore = defineStore("blocks", () => {
 });
 
 // we can use the expr to color in real time, and also correct syntax on the fly
+// we need a different audio context for each block
+// because we need to stop them independently
