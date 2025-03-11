8b5ecc72d62973663529ff9424bb3e2294ba823d
santiago <ry.yrupailla@gmail.com>
2025-03-11 18:05:04 -0500

    Remove unused code

--- src/main.ts
+++ src/main.ts
@@ -5,36 +5,9 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-import {
-  ChordComponent,
-  NoteComponent,
-  PrinterVisitor,
-  SerializerVisitor,
-} from "./domain/code_compositor/components";
-import { AudioPlayer, Duration, Pitch } from "./domain/audio-player";
-import { Tokenizer } from "./domain/code_compositor/tokenizer";
-import { Interpreter } from "./domain/code_compositor/interpreter";
-import { AudioComposition } from "./domain/code_compositor/audio-composition";
-
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
-
-// note.visit(new AudioPlayerVisitor());
-
-// const chord = new ChordComponent([
-//   {
-//     pitch: new Pitch("C"),
-//     duration: new Duration(),
-//   },
-//   {
-//     pitch: new Pitch("F"),
-//     duration: new Duration(),
-//   },
-// ]);
-
-// chord.visit(new AudioPlayerVisitor());
 
 app.mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import PlayerView from "../views/PlayerView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: PlayerView,
     },
     {
       path: "/about",
@@ -16,11 +16,6 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
-    },
-    {
-      path: "/player",
-      name: "player",
-      component: () => import("../views/PlayerView.vue"),
     },
   ],
 });
