cca311cf89b83bfd8be71fa17bc91725305bf323
santiago <ry.yrupailla@gmail.com>
2025-03-12 13:19:39 -0500

    Preloaded compositins

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -4,6 +4,17 @@ import { useBlocksStore } from "@/stores/blocks";
 import AceEditor from "./AceEditor.vue";
 
 const blocksStore = useBlocksStore();
+const preloadedCompositions = [
+  {
+    text: "(<c d4>2 c'x20 c'')",
+  },
+  {
+    text: "(c1x3)",
+  },
+  {
+    text: "(c1x3)",
+  },
+];
 
 async function play(text: string) {
   await new ExpressionPlayer(text).play();
@@ -13,15 +24,19 @@ async function play(text: string) {
 <template>
   <div class="container mx-auto max-w-4xl">
     <h1 class="text-3xl my-4">Player</h1>
-    <AceEditor />
-    <div class="" v-for="block in blocksStore.blocks">
-      <div class="card" v-for="node in block.nodes">
-        <div class="mockup-code">
-          <code class="pl-5">
-            {{ node.text }}
-          </code>
+    <div class="grid grid-cols-2 gap-2">
+      <div>
+        <AceEditor />
+      </div>
+      <div class="pt-6">
+        <div class="card mb-2" v-for="comp in preloadedCompositions">
+          <div class="mockup-code">
+            <code class="pl-5">
+              {{ comp.text }}
+            </code>
+          </div>
+          <button class="btn" @click="play(comp.text)">play</button>
         </div>
-        <button class="btn" @click="play(node.text)">play</button>
       </div>
     </div>
   </div>
